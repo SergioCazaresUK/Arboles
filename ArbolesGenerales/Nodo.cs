58faedea9f22ef4a426d3376aac0e9f90ab1dbfe
Sergio Armando Cazares Ruiz <Cazares250@outlook.com>
2022-05-28 12:58:59 -0700

    Nodo Construido

---
+++ ArbolesGenerales/Nodo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArbolesGenerales
+{
+    public class Nodo
+    {
+        //prop
+        public string Dato { get; set; }
+        public Nodo Hijo { get; set; }
+        public Nodo Hermano { get; set; }
+        //ctor
+        public Nodo(string dato, Nodo hijo = null, Nodo hermano = null)
+        {
+            Dato = dato;
+            Hijo = hijo;
+            Hermano = hermano;
+        }
+    }
+}
