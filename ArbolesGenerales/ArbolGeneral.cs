6d3e4e370ef30f70a0a55344e295db58c74277ac
Sergio Armando Cazares Ruiz <Cazares250@outlook.com>
2022-05-28 13:32:05 -0700

    Insertar Hijo

---
+++ ArbolesGenerales/ArbolGeneral.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArbolesGenerales
+{
+    public class ArbolGeneral
+    {
+        private readonly Nodo raiz;
+
+        //public Nodo Raiz => raiz;
+        public Nodo Raiz { get { return raiz; } }
+
+        public ArbolGeneral(string dato)
+        {
+            raiz = new Nodo(dato);
+        }
+
+        public Nodo InsertarHijo(Nodo padre, string dato)
+        {
+            if (string.IsNullOrWhiteSpace(dato))
+            {
+                throw new Exception("El Dato esta Vacio");
+            }
+
+            if(padre is null)
+            {
+                throw new Exception("El padre no puede ser nulo");
+            }
+
+            if(padre.Hijo is null)
+            {
+                padre.Hijo = new Nodo(dato);
+                return padre.Hijo;
+            }
+        }
+    }
+}
