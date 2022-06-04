ab9e53f46022b8b4866487c056101cdb58bb08b5
Sergio Armando Cazares Ruiz <Cazares250@outlook.com>
2022-06-04 14:32:17 -0700

    Codigo Terminado

--- ArbolesGenerales/ArbolGeneral.cs
+++ ArbolesGenerales/ArbolGeneral.cs
@@ -45,5 +45,73 @@ namespace ArbolesGenerales
                 return hijoactual.Hermano;
             }
         }
+        // investigar inmutabilidad
+        private void Recorrer(Nodo nodo, ref int posicion, ref string datos) 
+        { 
+            if(nodo != null)
+            {
+                string dato = nodo.Dato;
+                int cantidadGuiones = dato.Length + posicion;
+                string datoGuiones = dato.PadLeft(cantidadGuiones, '-');
+                datos += $"{datoGuiones}\n";
+
+                if(nodo.Hijo != null)
+                {
+                    posicion++;
+                    Recorrer(nodo.Hijo, ref posicion, ref datos);
+                    posicion--;
+                }
+                
+                if(nodo.Hermano != null && posicion != 0)
+                {
+                    Recorrer(nodo.Hermano, ref posicion, ref datos);
+                }
+            }
+        }
+
+        public string ObtenerArbol(Nodo nodo = null)
+        {
+            if(nodo is null)
+            {
+                nodo = raiz;
+            }
+            int posicion = 0;
+            string datos = string.Empty;
+
+            Recorrer(nodo, ref posicion, ref datos);
+            return datos;
+        }
+
+        public Nodo Buscar(string dato, Nodo nodoBusqueda)
+        {
+            if(nodoBusqueda is null)
+            {
+                nodoBusqueda = raiz;
+            }
+            
+            if(nodoBusqueda.Dato.ToUpper() == dato.ToUpper())
+            {
+                return nodoBusqueda;
+            }
+
+            if(nodoBusqueda.Hijo != null)
+            {
+                Nodo nodoEncontrado = Buscar(dato, nodoBusqueda.Hijo);
+                if(nodoEncontrado is null)
+                {
+                    return nodoEncontrado;
+                }
+            }
+
+            if(nodoBusqueda.Hermano != null)
+            {
+                Nodo nodoEncontrado = Buscar(dato, nodoBusqueda.Hermano);
+                if(nodoEncontrado is null)
+                {
+                    return nodoEncontrado;
+                }
+            }
+            return null;
+        }
     }
 }
--- ArbolesGenerales/Program.cs
+++ ArbolesGenerales/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using ArbolesGenerales;
+
+ArbolGeneral arbol = new ArbolGeneral("A");
+
+Nodo nodoB = arbol.InsertarHijo(arbol.Raiz, "B");
+Nodo nodoC = arbol.InsertarHijo(arbol.Raiz, "C");
+Nodo nodoD = arbol.InsertarHijo(nodoB, "D");
+Nodo nodoE = arbol.InsertarHijo(nodoD, "E");
+Nodo nodoF = arbol.InsertarHijo(nodoD, "F");
+
+
+string arbolCompleto = arbol.ObtenerArbol();
+Console.WriteLine(arbolCompleto);
