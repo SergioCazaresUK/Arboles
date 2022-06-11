1d4732b8d807c090258dfc48e7fb68da4014d37c
Sergio Armando Cazares Ruiz <Cazares250@outlook.com>
2022-06-11 12:07:49 -0700

    Agregue Insertar

---
+++ ArbolesBinarios/ArbolBinario.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArbolesBinarios
+{
+    public class ArbolBinario
+    {
+        private readonly Nodo raiz;
+        public Nodo Raiz { get { return raiz; } }
+        public ArbolBinario(string dato)
+        {
+            raiz = new Nodo(dato);
+        }
+
+        public enum TipoRecorrido
+        {
+            Preorden,
+            Inorden,
+            Posorden
+        }
+
+        public void Insertar (int dato, Nodo nodo = null)
+        {
+            if(nodo == null)
+            {
+                nodo = this.raiz;
+            }
+
+            if(dato < nodo.Dato)
+            {
+                if(nodo.SubArbolIzq == null)
+                {
+                    nodo.SubArbolIzq = new Nodo(dato);
+                }  
+                Insertar(dato, nodo.SubArbolIzq);
+            }
+
+            else if(dato > nodo.Dato)
+            {
+                if(nodo.SubArbolDer == null)
+                {
+                    nodo.SubArbolDer = new Nodo(dato);
+                }
+                Insertar(dato, nodo.SubArbolDer);
+            }
+
+        }
+    }
+}
---
+++ ArbolesBinarios/Nodo.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArbolesBinarios
+{
+    public class Nodo
+    {
+        public int Dato { get; set; }
+        public Nodo SubArbolIzq { get; set; }
+        public Nodo SubArbolDer { get; set; }
+
+        public Nodo(int dato, Nodo subArbolIzq = null, Nodo subArbolDer = null)
+        {
+            Dato = dato;
+            SubArbolIzq = subArbolIzq;
+            SubArbolIzq = subArbolIzq;
+        }
+    }
+}
