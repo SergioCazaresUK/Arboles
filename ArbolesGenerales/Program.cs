b754e96de6f1efa94df0f42adadf7ac5a296e12a
Sergio Armando Cazares Ruiz <Cazares250@outlook.com>
2022-06-11 11:14:07 -0700

    Creacion de Arboles Binarios

--- ArbolesGenerales/Program.cs
+++ ArbolesGenerales/Program.cs
@@ -7,7 +7,7 @@ Nodo nodoC = arbol.InsertarHijo(arbol.Raiz, "C");
 Nodo nodoD = arbol.InsertarHijo(nodoB, "D");
 Nodo nodoE = arbol.InsertarHijo(nodoD, "E");
 Nodo nodoF = arbol.InsertarHijo(nodoD, "F");
-
+Nodo nodoG = arbol.Buscar("D", nodoB);
 
 string arbolCompleto = arbol.ObtenerArbol();
 Console.WriteLine(arbolCompleto);
